91d7ba2b0e78f123b1519540b4f386c07e55bd04
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2025-10-29 22:02:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (89.75%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.c
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.c
@@ -1,9 +1,29 @@
 int smallestNumber(int n) {
-    int arr[10] = {1,3,7,15,31,63,127,255,511,1023};
-    int i;
-    for(i=0;i<10;i++){
-        if(n <=arr[i])
-            break;
-    }
-    return arr[i];
+    // int arr[10] = {1,3,7,15,31,63,127,255,511,1023};
+    // int i;
+    // for(i=0;i<10;i++){
+    //     if(n <=arr[i])
+    //         break;
+    // }
+    // return arr[i];
+    if(n==1)
+        return 1;
+    else if(n <= 3)
+        return 3;
+    else if(n <=7)
+        return 7;
+    else if(n<=15)
+        return 15;
+    else if(n<=31)
+        return 31;
+    else if(n<=63)
+        return 63;
+    else if(n<=127)
+        return 127;
+    else if(n<=255)
+        return 255;
+    else if(n<=511)
+        return 511;
+    
+        return 1023;
 }
