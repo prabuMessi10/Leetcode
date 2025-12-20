ca19ab753bf1454956b38a529ebfe2c23ab42024
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2025-12-21 05:29:35 +0530

    Time: 8 ms (74.88%) | Memory: 47.2 MB (25.44%) - LeetSync

---
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int count = 0;
+        int Alength = strs.length;
+        int wlength = strs[0].length();
+        for(int i=0;i<wlength;i++){
+            for(int j=0;j<Alength-1;j++){
+                if(strs[j].charAt(i) > strs[j+1].charAt(i)){
+                    count++;
+                    break;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
