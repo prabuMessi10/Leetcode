0cf44e14377716c861c5291837a9839c3348ab49
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2025-12-29 07:31:19 +0530

    Time: 0 ms (100.00%) | Memory: 80.6 MB (5.73%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,17 +1,29 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int i =0;
-        boolean peek = false;
-        int result = -1;
-        for(;i<arr.length-1;i++){
-            if(peek && arr[i] < arr[i+1]){
-               return -1; 
+        // int i =0;
+        // boolean peek = false;
+        // int result = -1;
+        // for(;i<arr.length-1;i++){
+        //     if(peek && arr[i] < arr[i+1]){
+        //        return -1; 
+        //     }
+        //     if(arr[i] > arr[i+1] && !peek){
+        //         peek=true;
+        //         result = i;
+        //     }
+        // }
+        // return result;
+        int start = 0;
+        int end = arr.length-1;
+        while(start < end){
+            int mid = start + (end-start)/2;
+            if(arr[mid] < arr[mid+1]){
+                start = mid+1;
             }
-            if(arr[i] > arr[i+1] && !peek){
-                peek=true;
-                result = i;
+            else{
+                end = mid;
             }
         }
-        return result;
+        return start;
     }
 }
