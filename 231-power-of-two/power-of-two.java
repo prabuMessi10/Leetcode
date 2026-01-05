34db8c8b2a222610cb3a2c54dc5b0aae56d81e32
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2026-01-05 13:59:11 +0530

    Time: 1 ms (95.55%) | Memory: 42.6 MB (54.41%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==1 || n==2 )
+            return true;
+        if(n==0)
+            return false;
+        while(n!=1 && n!=0){
+            if(n%2 !=0)
+                return false;
+            n/=2;
+        }
+        return true;
+    }
+}
