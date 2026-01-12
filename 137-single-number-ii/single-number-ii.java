355a1cbe6fda4be85a2e4fb01047c07eb147184a
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2026-01-12 21:31:45 +0530

    Time: 6 ms (42.26%) | Memory: 46.9 MB (6.15%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,16 +1,38 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int i;
-        for( i=0;i<nums.length-1;i++){
-            if(nums[i] == nums[i+1] && nums[i+1] == nums[i+2]){
-                i+=2;
-            }
-            else{
-                
-                return nums[i];
-            }
+        HashMap<Integer , Integer> set = new HashMap<>();
+        for(int n : nums){
+            set.put(n, set.getOrDefault(n,0)+1);
         }
-        return nums[i];
+        for(int key : set.keySet()){
+            if(set.get(key) == 1)
+                return key;
+        }
+        return 0;
+
+
+
+
+
+
+
+
+
+
+
+        // Arrays.sort(nums);
+        // int i;
+        // for( i=0;i<nums.length-1;i++){
+        //     if(nums[i] == nums[i+1] && nums[i+1] == nums[i+2]){
+        //         i+=2;
+        //     }
+        //     else{
+        //         if(nums[i] == nums[i+1])
+        //             return nums[i+2];
+        //         else
+        //         return nums[i];
+        //     }
+        // }
+        // return nums[i];
     }
 }
