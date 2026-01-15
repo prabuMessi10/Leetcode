f1c526fb5197858296246b51715f3385b13a1d64
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2026-01-15 19:44:05 +0530

    Time: 23 ms (12.14%) | Memory: 47.4 MB (11.65%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,21 +1,28 @@
 class Solution {
     public String reverseWords(String s) {
-        int start = 0;
-        int end = 0;
-        String result = "";
-        for(int i=0;i<s.length();i++){
-            if((s.charAt(i) == ' ') || (i == s.length() - 1)){
-                end = i;
-                if(i==s.length()-1)
-                    result+=' ';
-                while(end >= start){
-                    result+=s.charAt(end);
-                    end--;
-                }
-                start = i+1;
+        // int start = 0;
+        // int end = 0;
+        // String result = "";
+        // for(int i=0;i<s.length();i++){
+        //     if((s.charAt(i) == ' ') || (i == s.length() - 1)){
+        //         end = i;
+        //         if(i==s.length()-1)
+        //             result+=' ';
+        //         while(end >= start){
+        //             result+=s.charAt(end);
+        //             end--;
+        //         }
+        //         start = i+1;
                 
-            }
+        //     }
+        // }
+        // return result.trim();
+        String result ="";
+        String[] A = s.split("\\s+");
+        for(String S : A){
+            StringBuilder set = new StringBuilder(S);
+            result += set.reverse()+" ";
         }
-        return result.trim();
+        return  result.trim();
 }
 }
