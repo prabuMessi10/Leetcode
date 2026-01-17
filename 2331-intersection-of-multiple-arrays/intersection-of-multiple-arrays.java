713db52ac0615e6578f460e5fb7bd571a8a832f3
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2026-01-17 20:37:57 +0530

    Time: 2 ms (99.87%) | Memory: 46.6 MB (50.72%) - LeetSync

--- 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.java
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.java
@@ -1,17 +1,27 @@
 class Solution {
     public List<Integer> intersection(int[][] arr) {
         List<Integer> result = new ArrayList<>();
-       Map<Integer , Integer> set = new HashMap<>();
-       for(int i=0;i<arr.length;i++){
-        for(int j=0;j<arr[i].length;j++){
-            set.put(arr[i][j],set.getOrDefault(arr[i][j],0)+1);
+    //    Map<Integer , Integer> set = new HashMap<>();
+    //    for(int i=0;i<arr.length;i++){
+    //     for(int j=0;j<arr[i].length;j++){
+    //         set.put(arr[i][j],set.getOrDefault(arr[i][j],0)+1);
+    //     }
+    //    }
+    //    for(int n : set.keySet()){
+    //         if(set.get(n) == arr.length)
+    //             result.add(n);
+    //    }
+    //    Collections.sort(result);
+    //    return result;
+        int[] count = new int[1001];
+        for(int[] row : arr){
+            for(int n : row)
+                count[n]++;
         }
-       }
-       for(int n : set.keySet()){
-            if(set.get(n) == arr.length)
-                result.add(n);
-       }
-       Collections.sort(result);
-       return result;
+        for(int i=0;i<count.length;i++){
+            if(count[i] == arr.length)
+                result.add(i);
+        }
+        return result;
     }
 }
