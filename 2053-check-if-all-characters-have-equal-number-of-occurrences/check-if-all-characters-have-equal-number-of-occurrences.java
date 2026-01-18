765af96a7371c6c3650d1ede3ae510bb0c45f9ee
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2026-01-18 08:58:35 +0530

    Time: 3 ms (59.99%) | Memory: 43.1 MB (87.72%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
@@ -1,14 +1,25 @@
 class Solution {
     public boolean areOccurrencesEqual(String s) {
-        Map<Character, Integer> set = new HashMap<>();
-        for(char n : s.toCharArray()){
-            set.put(n,set.getOrDefault(n,0)+1);
-        }
-        int digit = set.get(s.charAt(0));
-        for(char n : set.keySet()){
-            if(digit != set.get(n))
-                return false;
-        }
-        return true;
+    //     Map<Character, Integer> set = new HashMap<>();
+    //     for(char n : s.toCharArray()){
+    //         set.put(n,set.getOrDefault(n,0)+1);
+    //     }
+    //     int digit = set.get(s.charAt(0));
+    //     for(char n : set.keySet()){
+    //         if(digit != set.get(n))
+    //             return false;
+    //     }
+    //     return true;
+    // }
+    int[] count = new int[26];
+    for(int i=0;i<s.length();i++){
+        count[s.charAt(i) - 'a']++;
+    }
+    int digit = count[s.charAt(0) - 'a'];
+    for(int i=1;i<s.length();i++){
+        if(digit != count[s.charAt(i) - 'a'])
+            return false;
+    }
+    return true;
     }
 }
