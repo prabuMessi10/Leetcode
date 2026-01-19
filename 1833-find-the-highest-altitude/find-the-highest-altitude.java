3d84b71248926be7e96ea57cdf607f0049f7e91c
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2026-01-19 18:27:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (62.91%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max = gain[0];
+        for(int i=1;i<gain.length;i++){
+            gain[i] +=gain[i-1];
+            if(gain[i] > max)
+                max = gain[i];
+        }
+        
+        return (max > 0) ? max : 0;
+    }
+}
