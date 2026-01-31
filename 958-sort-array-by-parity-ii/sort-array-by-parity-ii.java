a2c2a5ff4e7fcb0cc65d9170aa1e7de2f34f822c
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2026-01-31 14:32:01 +0530

    Time: 2 ms (99.27%) | Memory: 48.7 MB (43.97%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.java
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.java
@@ -1,24 +1,37 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
-        int[] odd = new int[nums.length/2];
-        int[] even = new int[nums.length/2];
-        int o=0,e=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]%2==0)
-                even[e++] = nums[i];
-            else
-                odd[o++] = nums[i];
+        // int[] odd = new int[nums.length/2];
+        // int[] even = new int[nums.length/2];
+        // int o=0,e=0;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i]%2==0)
+        //         even[e++] = nums[i];
+        //     else
+        //         odd[o++] = nums[i];
+        // }
+        // o=0;
+        // e=0;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(i%2==0)
+        //         nums[i] = even[e++];
+        //     else
+        //         nums[i] = odd[o++];
+        // }
+        // return nums;
+        int[] result = new int[nums.length];
+        int odd =1, even =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                result[even] = nums[i];
+                even+=2;
+            }
+            else{
+                result[odd] = nums[i];
+                odd+=2;
+            }
         }
-        o=0;
-        e=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(i%2==0)
-                nums[i] = even[e++];
-            else
-                nums[i] = odd[o++];
-        }
-        return nums;
+        return result;
     }
 }
