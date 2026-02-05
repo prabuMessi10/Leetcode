91566a937854b37c480fc66e08eb000e630597fa
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2026-02-05 22:44:00 +0530

    Time: 3 ms (99.44%) | Memory: 202.9 MB (18.35%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -10,32 +10,43 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        if(head==null || head.next==null)
-            return null;
+        // if(head==null || head.next==null)
+        //     return null;
         
-        if(head.next.next==null){
-            head.next=null;
-            return head;
-        }
-        int count =0;
+        // if(head.next.next==null){
+        //     head.next=null;
+        //     return head;
+        // }
+        // int count =0;
+        // ListNode slow = head;
+        // while(slow!=null){
+        //     count++;
+        //     slow = slow.next;
+        // }
+        // ListNode fast = head;
+        // slow = head;
+        // while(fast!=null&&fast.next!=null && fast.next.next!=null)
+        // {
+        //     fast=fast.next.next;
+        //     slow = slow.next;
+        // }
+        // if(count%2==0)
+        //     slow.next = slow.next.next;
+        // else{
+        //     slow.val = slow.next.val;
+        //     slow.next = slow.next.next;
+        // }
+        // return head;
+        if(head.next==null)
+            return null;
         ListNode slow = head;
-        while(slow!=null){
-            count++;
-            slow = slow.next;
-        }
-        ListNode fast = head;
-        slow = head;
-        while(fast!=null&&fast.next!=null && fast.next.next!=null)
-        {
-            fast=fast.next.next;
-            slow = slow.next;
-        }
-        if(count%2==0)
+        ListNode fast = head.next.next;
+        while(fast!=null && fast.next!=null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
             slow.next = slow.next.next;
-        else{
-            slow.val = slow.next.val;
-            slow.next = slow.next.next;
-        }
-        return head;
+            return head;
     }
 }
