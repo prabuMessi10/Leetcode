cea01ceccb05dfb76cfc8fe8c219d6ed1f3119c2
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2026-03-04 23:30:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (93.03%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,12 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        String res = "";
-        for(int i=0;i<s.length();i++){
-            if(Character.isUpperCase(s.charAt(i)))
-                res+=Character.toLowerCase(s.charAt(i));
-            else
-                res+=s.charAt(i);
-        }
-        return res;
+      return s.toLowerCase();
     }
 }
