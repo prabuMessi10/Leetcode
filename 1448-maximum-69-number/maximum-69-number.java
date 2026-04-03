dbd21ce9c967751e1739dcd9a39c82c69b1b3c6a
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2026-04-03 10:34:10 +0530

    Time: 5 ms (2.84%) | Memory: 42.3 MB (36.87%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,24 +1,16 @@
 class Solution {
     public int maximum69Number (int num) {
         String a = num+"";
-        ArrayList<Integer> ans = new ArrayList<>();
+        int summa =-1;
+        String res ="";
         for(int i=0;i<a.length();i++){
-            String res ="";
-            for(int j=0;j<a.length();j++){
-                if(j==i){
-                    res += (a.charAt(j) =='6') ? '9' : '6';
-                }
-                else{
-                    res+=a.charAt(j);
-                }
+            if(summa ==-1 && a.charAt(i) == '6'){
+            res+='9';
+            summa++;
             }
-                System.out.println(res);
-                ans.add(Integer.parseInt(res));
+            else
+                res+=a.charAt(i);
         }
-        int max = num;
-        for(int n : ans){
-            if(max < n) max = n;
-        }
-        return max;
+        return Integer.parseInt(res);
     }
 }
