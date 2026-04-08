cd136346f04a8acbe90f44a9a8a7b9cdf48a2daf
prabuMessi10 <thannasiprabur.24cse@kongu.edu>
2026-04-08 09:11:30 +0530

    Time: 6 ms (39.43%) | Memory: 45.6 MB (77.22%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean judgeCircle(String moves) {
+        if(moves.length() % 2 != 0) return false;
         int hori =0;
         int ver = 0;
         for(int i=0;i<moves.length();i++){
