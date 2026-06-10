0b23db9ad3e1054a10c63dc054a853534dfc4b2d
prabuMessi10 <192571901+prabuMessi10@users.noreply.github.com>
2026-06-10 14:17:28 +0530

    Time: 1 ms (87.73%) | Memory: 45.6 MB (29.44%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,15 +1,15 @@
 class Solution {
-    public void permutation(int[]nums, boolean[]check,List<List<Integer>>res,List<Integer> ds){
-        if(ds.size()==nums.length){
+    public void permutation(int i,int[] nums,List<List<Integer>> res , List<Integer> ds,boolean[] check){
+        if(i == nums.length){
             res.add(new ArrayList<>(ds));
             return;
         }
-        for(int ind=0;ind<nums.length;ind++){
-            if(check[ind]==false){
-                check[ind] = true;
-                ds.add(nums[ind]);
-                permutation(nums,check,res,ds);
-                check[ind]=false;
+        for(int j=0;j<nums.length;j++){
+            if(check[j] == false){
+                ds.add(nums[j]);
+                check[j] = true;
+                permutation(i+1,nums,res,ds,check);
+                check[j] = false;
                 ds.remove(ds.size()-1);
             }
         }
@@ -17,7 +17,7 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         boolean[] check = new boolean[nums.length];
-        permutation(nums,check,res,new ArrayList<>());
+        permutation(0,nums,res,new ArrayList<>(),check);
         return res;
     }
 }
