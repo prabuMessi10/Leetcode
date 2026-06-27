dee2c2ce847a8d5c22ad7b6fe8433bd1a3c12e5d
prabuMessi10 <192571901+prabuMessi10@users.noreply.github.com>
2026-06-27 22:51:54 +0530

    Time: 2 ms (95.89%) | Memory: 45.7 MB (50.42%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,21 @@
 class Solution {
-    public void combination(int i , int target , int[] candidates,List<List<Integer>> set , List<Integer> ds ){
+    public void solution(int i,int[] candidates, int target,List<List<Integer>> ans , List<Integer> ds){
         if(i == candidates.length){
             if(target == 0){
-                set.add(new ArrayList<>(ds));
+                ans.add(new ArrayList<>(ds));
             }
             return;
         }
-        if(candidates[i] <= target){
-            ds.add(candidates[i]);
-            combination(i,target-candidates[i],candidates,set,ds);
-            ds.remove(ds.size()-1);
-        }
-        combination(i+1,target,candidates,set,ds);
+       if(target >= candidates[i]){
+        ds.add(candidates[i]);
+        solution(i,candidates,target-candidates[i],ans,ds);
+        ds.remove(ds.size()-1);
+       }
+       solution(i+1,candidates,target,ans,ds);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> set = new ArrayList<>();
-        combination(0,target,candidates,set,new ArrayList<Integer>());
-        return set;
+        List<List<Integer>> ans = new ArrayList<>();
+        solution(0,candidates,target,ans,new ArrayList<>());
+        return ans;
     }
 }
